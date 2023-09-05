8b76f6407e151e68c4cd97801d9069305fc80199
tharunisaripilli <144013739+tharunisaripilli@users.noreply.github.com>
2023-09-05 14:51:03 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -4,7 +4,7 @@ class Add
     public static void main(String []args)
     {
         int x=3, y=5;
-        int z;
+        int z=x+y;
         System.out.println("values of x and y:"+x+" "+y);
         System.out.println("addition of x and y"+z);
     }
