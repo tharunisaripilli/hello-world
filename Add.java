a101d8099f9e8fff363ffc01c74de4fd674cd859
tharunisaripilli <tharunisaripilli3@gmail.com>
2023-09-05 13:48:49 +0530

    addition of numbers

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class Add
+{
+    pulic static void main(String []args)
+    {
+        int x=3, y=5;
+        int z;
+        System.out.println("values of x and y:"+x+" "+y);
+        System.out.println("addition of x and y"+z);
+    }
+}
